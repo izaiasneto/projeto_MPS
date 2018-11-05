d6f16a61ad4ad97377c518c816a6e0c178b717b0
glauber <glauberufpb@gmail.com>
2018-11-05 21:00:23 -0200

    ..

---
+++ src/business/control/Memento.java
@@ -0,0 +1,14 @@
+package business.control;
+
+public class Memento {
+
+	protected String estado;
+	 
+    public Memento(String texto) {
+        estado = texto;
+    }
+ 
+    public String getTextoSalvo() {
+        return estado;
+    }
+}
